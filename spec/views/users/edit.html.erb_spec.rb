a4db7539f451d440097366f5f1531523917305aa
nakaty <78027524+naktty@users.noreply.github.com>
2025-06-15 08:43:31 +0900

    テストコードから不要な空行を削除

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe "users/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", user_path(user), "post" do
-
       assert_select "input[name=?]", "user[name]"
 
       assert_select "input[name=?]", "user[email]"
--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe "users/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", users_path, "post" do
-
       assert_select "input[name=?]", "user[name]"
 
       assert_select "input[name=?]", "user[email]"
